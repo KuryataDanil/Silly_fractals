5bf7afbc967a1fb4d42cdd6d776d77d51948c714
DanilK17 <danil.17.4.2004@gmail.com>
2023-11-20 19:35:34 +0300

    add main UI
    add UI and settings, music

---
+++ Silly_fractals/Assets/Scripts/UI Scripts/ButtonManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void SwitchScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+
+}
